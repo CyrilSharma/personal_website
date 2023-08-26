d0ff579a63768288fe9371d5b30a9550fb41ee11
CyrilSharma <sharma.cyril@gmail.com>
2023-08-26 16:24:33 -0400

    crucial update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
     return (
       <div className="flex flex-row mt-5 justify-center">
-        <img className="w-80 h-80 object-cover" alt="No Cactus :((" src="/Cactus.gif"></img>
+        <img className="w-80 h-80 object-cover -scale-x-100" alt="No Cactus :((" src="/Cactus.gif"></img>
         <div>
             <h1 className="header text-center">Hi!</h1>
             <p className="paragraph text-center">
