ca9775a5f57c89dac9973fb9006e136128dd5633
CyrilSharma <sharma.cyril@gmail.com>
2023-08-26 16:07:18 -0400

    this is the greatest thing i have ever done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import NavBar from '../components/NavBar'
+import Image from 'next/image';
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 export default function HomePage() {
     return (
-      <div>
-        <h1 className="header text-center">Hi!</h1>
-        <p className="paragraph text-center">
-            {"I'm Cyril, I'm interested in currently interested in AI, Compilers, HPC, and Algorithms more broadly."}
-        </p>
-        <p className="paragraph text-center">
-            {"I also enjoy playing Badminton, crushing my opponents in Foosball, and reading."}
-        </p>
+      <div className="flex flex-row mt-5 justify-center">
+        <img className="w-80 h-80 object-cover" alt="No Cactus :((" src="/Cactus.gif"></img>
+        <div>
+            <h1 className="header text-center">Hi!</h1>
+            <p className="paragraph text-center">
+                {"I'm Cyril, I'm interested in currently interested in AI, Compilers, HPC, and Algorithms more broadly."}
+            </p>
+            <p className="paragraph text-center">
+                {"I also enjoy playing Badminton, crushing my opponents in Foosball, and reading."}
+            </p>
+        </div>
+        <img className="w-80 h-80 object-cover" alt="No Cactus :((" src="/Cactus.gif"></img>
       </div>
     );
   }
