bd9fed4e8f6aeb67227434ec20af5e3c5a17b322
CyrilSharma <sharma.cyril@gmail.com>
2023-08-26 15:00:43 -0400

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function HomePage() {
       <div>
         <h1 className="header text-center">Hi!</h1>
         <p className="paragraph text-center">
-            I'm Cyril, I'm interested in currently interested in AI, Compilers, HPC, and Algorithms more broadly.
+            {"I'm Cyril, I'm interested in currently interested in AI, Compilers, HPC, and Algorithms more broadly."}
         </p>
         <p className="paragraph text-center">
-            I also enjoy playing Badminton, crushing my opponents in Foosball, and reading.
+            {"I also enjoy playing Badminton, crushing my opponents in Foosball, and reading."}
         </p>
       </div>
     );
