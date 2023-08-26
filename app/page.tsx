48b52304370a4ddc255107c122935d08919604c6
CyrilSharma <sharma.cyril@gmail.com>
2023-08-26 16:16:37 -0400

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
         <div>
             <h1 className="header text-center">Hi!</h1>
             <p className="paragraph text-center">
-                {"I'm Cyril, I'm interested in currently interested in AI, Compilers, HPC, and Algorithms more broadly."}
+                {"I'm Cyril, I'm interested in AI, Compilers, HPC, and Algorithms more broadly."}
             </p>
             <p className="paragraph text-center">
                 {"I also enjoy playing Badminton, crushing my opponents in Foosball, and reading."}
